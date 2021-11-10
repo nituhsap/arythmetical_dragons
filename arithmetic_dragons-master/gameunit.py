58a4f1c02c3ef0b507fdfbb8973c9ce581b78d82
nituhsap <nituhsap@mail.ru>
2021-11-10 22:17:41 +0300

    add: gameunit & hero files

--- arithmetic_dragons-master/gameunit.py
+++ arithmetic_dragons-master/gameunit.py
@@ -1,14 +1,25 @@
 # coding: utf-8
 # license: GPLv3
 
+from abc import ABC, abstractmethod
 
-class Attacker:
-    _health = None
-    _attack = None
+class Mobile(ABC):
+	pass
 
-    def attack(self, target):
-        target._health -= self._attack
 
-    def is_alive(self):
-        return self._health > 0
-    
+class Attacker(Mobile):
+	_health = None
+	_attack = None
+        
+	def is_alive(self):
+		return self._health > 0
+
+	def attack(self, target):
+		target._health -= self._attack   
+        
+class Helper(Mobile):
+	_heal = None
+	
+	def heal(self, target):
+		if target._health < 100:
+			target._health += self._heal
